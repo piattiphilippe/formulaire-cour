3aad5642ade15b02d1b49467ebfcee257ae94867
philippe piatti <philippe_piatti@orange.fr>
2018-12-30 20:10:26 +0100

    rajout toggle sur h1

---
+++ app.js
@@ -0,0 +1,8 @@
+let rouge = document.querySelector("h1");
+// let rougi = document.querySelector("button");
+    rouge.addEventListener("click",changeCouleur);
+
+
+    function changeCouleur(){
+        rouge.classList.toggle("roug")
+    }
